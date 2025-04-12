bed8e05cdedb62282bb1339130baa125ff5d8213
chunjimin-1 <chunjimin@ajou.ac.kr>
2025-04-12 20:49:04 +0900

    Merge: kept main version of EnemyBehavior.cs and GameManager.cs

---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public int enemyCount = 5;
+    public Transform player;
+
+    void Start()
+    {
+        SpawnEnemies();
+    }
+
+    void SpawnEnemies()
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            float x = Random.Range(GameManager.Instance.planeMin.x, GameManager.Instance.planeMax.x);
+            float z = Random.Range(GameManager.Instance.planeMin.y, GameManager.Instance.planeMax.y);
+            Vector3 spawnPos = new Vector3(x, 0f, z);
+
+            GameObject enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+            enemy.GetComponent<EnemyBehavior>().SetInitialDirection(player.position);
+        }
+    }
+}

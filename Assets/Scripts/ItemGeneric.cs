980c359de5e435e9d1b7a0d3b1f7e1089c72da8b
YoonByungSun <ssp31n@naver.com>
2025-04-19 19:37:54 +0900

    save

--- Assets/Scripts/ItemGeneric.cs
+++ Assets/Scripts/ItemGeneric.cs
@@ -4,11 +4,11 @@ public class ItemGeneric : MonoBehaviour
 {
     // 스폰 시 자동 설정됨. 수정 불필요
     public string itemName = "GenericItem";
-    public float rotateSpeed = 90f;
+    public Vector3 rotateSpeed = new Vector3(45f, 90f, 30f);
 
     void Update()
     {
-        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(rotateSpeed * Time.deltaTime, Space.World);
     }
 
     private void OnTriggerEnter(Collider other)

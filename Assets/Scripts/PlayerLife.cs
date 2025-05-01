8258923ed61095163f1607ffc523471a636922cd
송재혁 <jhsong1220@ajou.ac.kr>
2025-05-02 02:32:47 +0900

    animation complete
    그리고 아이템 진행중

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -5,25 +5,30 @@ public class PlayerLife : MonoBehaviour
     public static PlayerLife Instance;
 
     public int life = 3;
+    private PlayerAnimatorController animatorController;
 
     void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
-    }
 
+        animatorController = GetComponent<PlayerAnimatorController>();
+    }
 
     void Update()
     {
         if (life < 0)
         {
-            GameManager.Instance.GameOver(); // ✅ GameOver UI는 GameManager에서 처리
+            animatorController?.PlayPanic(); // 사망 애니메이션
+            GameManager.Instance.GameOver();
         }
     }
 
     public void TakeDamage()
     {
         life--;
+
+        animatorController?.PlayGetHit(); // 피격 애니메이션
 
         if (life >= 0)
         {
